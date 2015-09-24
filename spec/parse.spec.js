d73a8fe624d5df65d07e170c414db3fea681d96f
Gert K. Sønderby <gert@one.com>
2015-09-24 15:38:17 +0200

    Discard selectorless brace-enclosed blocks.
    (cherry picked from commit 0d732f5c9f656dbe3253d0eeadc52dfab623680c)
    
    Conflicts:
    	lib/parse.js
    	package.json
    	spec/parse.spec.js

--- spec/parse.spec.js
+++ spec/parse.spec.js
@@ -1028,6 +1028,63 @@ var TESTS = [
 
 			return result;
 		})()
+	},
+	{
+		input: 'body {color:red;}{color: green;font-size: 13px;}',
+		result: (function() {
+			var result = {
+				cssRules: [
+					{
+						selectorText: "body",
+						style: {
+							0: "color",
+							length: 1,
+							parentRule: "..",
+							color: "red"
+						},
+						parentRule: null,
+						parentStyleSheet: "../.."
+					}
+				],
+				parentStyleSheet: null
+			};
+			return result;
+		})()
+	},
+	{
+		input: 'body {color:red;}, *{color: green;font-size: 13px;}',
+		result: (function() {
+			var result = {
+				cssRules: [
+					{
+						selectorText: "body",
+						style: {
+							0: "color",
+							length: 1,
+							parentRule: "..",
+							color: "red"
+						},
+						parentRule: null,
+						parentStyleSheet: "../.."
+					},
+					{
+						parentRule: null,
+						parentStyleSheet: "../..",
+						selectorText: ", *",
+						style: {
+							0: "color",
+							1: "font-size",
+							length: 2,
+							parentRule: "..",
+							color: "green",
+							"font-size": "13px"
+						}
+					}
+				],
+				parentStyleSheet: null
+			};
+			return result;
+		})()
 	}
 ];
 
